d3dfb6f5c0317b5def049c11387c76e1d1aa5c06
shaco74 <kacper74@hotmail.com>
2022-06-27 12:32:47 +0200

    Zwischenstand Frontend

--- frontend/pages/image/index.tsx
+++ frontend/pages/image/index.tsx
@@ -1,7 +1,28 @@
 import { Input } from "@chakra-ui/react";
 import { Heading } from "@chakra-ui/react";
+import { readFile } from "fs";
 
 const imagePage = () => {
+  const onImageSelected = (e: any) => {
+    console.log(e.target.files[0]);
+    let file: File = e.target.files[0];
+
+    if (file.type === "image/png" || file.type === "image/jpeg") {
+      let reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        console.log("reader.result");
+        let e = reader.result;
+        console.log(typeof e);
+        console.log(e);
+      };
+      reader.onerror = function (error) {
+        console.log("Error: ", error);
+      };
+    } else {
+      console.log("File not supported");
+    }
+  };
   return (
     <div className="main">
       <div className="header">
@@ -10,12 +31,12 @@ const imagePage = () => {
         </Heading>
       </div>
       <main>
-        <Input type="file" />
+        <Input type="file" onChange={onImageSelected} />
       </main>
       <style jsx>{`
         .main {
           height: 100vh;
-          background-color: rgb(110, 110, 110);
+          background-color: rgb(224, 212, 159);
         }
         .header {
           padding: 20px 20px 30px 20px;
